00541a882004da220d0e9bb9e55431dcfa1f4782
JonnthanPortilla <jj.rosero@hotmail.com>
2024-01-02 16:00:47 -0500

    update2  ssh file

--- jenkins_deploy_prod_docker.sh
+++ jenkins_deploy_prod_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh ip172-18-0-80-cma6gi5np9tg00enrr8g@direct.labs.play-with-docker.com <<EOF
+ssh -T ip172-18-0-80-cma6gi5np9tg00enrr8g@direct.labs.play-with-docker.com <<EOF
   git pull
   cd docker1/
   docker compose up -d
