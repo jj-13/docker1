96bd069b0e870fa2b09820981ba5bfb5d33d7355
JonnthanPortilla <jj.rosero@hotmail.com>
2024-01-03 08:55:05 -0500

    fix sh file

--- jenkins_deploy_prod_docker.sh
+++ jenkins_deploy_prod_docker.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+
+git pull
+cd docker1/
+docker compose up -d
+
 #ssh -T root@buildkitsandbox <<EOF
-ssh -T ip172-18-0-38-cmalihao7r5g00avnp5g@direct.labs.play-with-docker.com <<EOF
-  git pull
-  cd docker1/
-  docker compose up -d
-  exit
-EOF
+#ssh -T ip172-18-0-38-cmalihao7r5g00avnp5g@direct.labs.play-with-docker.com <<EOF
+#  git pull
+#  cd docker1/
+#  docker compose up -d
+#  exit
+#EOF
