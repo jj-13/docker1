e28cf73ae750456a40515e6d3e5fbc1ea886b29f
JonnthanPortilla <jj.rosero@hotmail.com>
2024-01-03 08:27:13 -0500

    change ssh

--- jenkins_deploy_prod_docker.sh
+++ jenkins_deploy_prod_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-#ssh -T ip172-18-0-80-cma6gi5np9tg00enrr8g@direct.labs.play-with-docker.com <<EOF
-ssh -T root@buildkitsandbox <<EOF
+#ssh -T root@buildkitsandbox <<EOF
+ssh -T ip172-18-0-38-cmalihao7r5g00avnp5g@direct.labs.play-with-docker.com <<EOF
   git pull
   cd docker1/
   docker compose up -d
