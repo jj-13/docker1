86ee13723db2ccd8c3a0655265300dc150a03338
JonnthanPortilla <jj.rosero@hotmail.com>
2024-01-03 15:03:14 -0500

    new ssh pi

--- jenkins_deploy_prod_docker.sh
+++ jenkins_deploy_prod_docker.sh
@@ -3,7 +3,7 @@
 eval $(ssh-agent -s) && ssh-add jenkins_agent
 
 #ssh -T root@buildkitsandbox <<EOF
-ssh -o StrictHostKeyChecking=no -T ip172-18-0-38-cmalihao7r5g00avnp5g@direct.labs.play-with-docker.com <<EOF
+ssh -o StrictHostKeyChecking=no -T ip172-18-0-49-cmarr0dnp9tg009crrr0@direct.labs.play-with-docker.com <<EOF
   git pull
   cd docker1/
   docker compose up -d
